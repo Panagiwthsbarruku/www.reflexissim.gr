df246c614ca9d0997b1938281365f6e4dbcacc63
Barruku <141589210+Panagiwthsbarruku@users.noreply.github.com>
2024-03-08 21:02:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function login() {
+  var email = document.getElementById("user124").value;
+  var password = document.getElementById("6464").value;
+  // Έλεγχος εγκυρότητας
+  if (users.hasOwnProperty(email) && users[email] === password) {
+    var name = email.split('@')[0];
+    alert("Καλώς ήρθες, " + name + "!");
+    // Πρόσβαση στη σελίδα
+    window.location.href = "http://127.0.0.1:3000/before.html"; // Αντικαταστήστε το "welcome.html" με τη σελίδα προσβασης
+  } else {
+    alert("Άκυρα στοιχεία σύνδεσης. Παρακαλούμε δοκιμάστε ξανά.");
+  }
+}
